81b969317110b07d79f4630baa2cb5d982f16666
Ras96 <asymptote.k.k@gmail.com>
2022-02-23 09:20:47 +0900

    :wrench: brew install oh-my-posh

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,6 +14,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/i
 
 # 各種ツールのインストール
 # なるべくbrewで入れるようにする
+brew tap jandedobbeleer/oh-my-posh
 brew install \
   asdf \
   bat \
@@ -26,6 +27,7 @@ brew install \
   go \
   jq \
   live-server \
+  oh-my-posh \
   yarn
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
