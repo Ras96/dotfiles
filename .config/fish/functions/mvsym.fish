6340b8c9e608f7526d57ab3fd73617ddc9deabba
Ras96 <asymptote.k.k@gmail.com>
2022-02-23 09:14:43 +0900

    :wrench: Update mvsym

--- .config/fish/functions/mvsym.fish
+++ .config/fish/functions/mvsym.fish
@@ -1,6 +1,7 @@
 function mvsym
       set -x DOTFILES_DIR ~/ghq/github.com/Ras96/dotfiles
-      set -x ARG_FILE $argv[1]
-      mv ~/$ARG_FILE $DOTFILES_DIR/$ARG_FILE
-      ln -s $DOTFILES_DIR/$ARG_FILE ~/$ARG_FILE
+      set -x FILE $argv[1]
+      mv $HOME/$FILE $DOTFILES_DIR/$FILE
+      ln -s $DOTFILES_DIR/$FILE $HOME/$FILE
+      echo "ln -s \$DOTFILES_DIR/$FILE \$HOME/$FILE" >> $DOTFILES_DIR/scripts/symboliclinks.sh
 end
