1dcd1556f2641d726d81492ce647c1157ae2b2b1
Ras96 <asymptote.k.k@gmail.com>
2022-02-23 09:15:25 +0900

    :wrench: Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+DOTFILES_REPO=Ras96/dotfiles
+DOTFILES_REPO_URL=git@github.com:$DOTFILES_REPO.git
+
+# セットアップ & 基本コマンドのインストール
+yes | sudo apt update
+yes | sudo apt upgrade
+yes | sudo add-apt-repository ppa:git-core/ppa
+yes | sudo apt install build-essential curl file git procps
+
+# brewのインストール
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+# 各種ツールのインストール
+# なるべくbrewで入れるようにする
+brew install \
+  asdf \
+  bat \
+  fish \
+  fzf \
+  gh \
+  ghq \
+  git-delta \
+  gitmoji \
+  go \
+  jq \
+  live-server \
+  yarn
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# レポジトリのクローン
+ghq get $DOTFILES_REPO_URL
+
+# wsl.confのコピー
+sudo cp $(ghq root)/$DOTFILES_REPO/wsl.conf /etc/wsl.conf
+
+# asdfプラグインのインストール
+cat ~/.tool-versions | awk '{print "asdf plugin add " $1}' | sh
+asdf install
+
+# シンボリックリンクを貼る
+source $DOTFILES_REPO/scripts/symboliclinks.sh
+
+sudo sh -c "echo $(which fish) >> /etc/shells"
+sudo chsh -s $(which fish)
---
+++ scripts/symboliclinks.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DOTFILES_REPO=Ras96/dotfiles
+DOTFILES_DIR=$(ghq root)/$DOTFILES_REPO
+
+ln -s $DOTFILES_DIR/.config/bat $HOME/.config/bat
+ln -s $DOTFILES_DIR/.config/fish $HOME/.config/fish
+ln -s $DOTFILES_DIR/.config/gitmoji-nodejs $HOME/.config/gitmoji-nodejs
+ln -s $DOTFILES_DIR/.config/yarn $HOME/.config/yarn
+ln -s $DOTFILES_DIR/.wincmd $HOME/.wincmd
+ln -s $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES_DIR/.golangci.yml $HOME/.golangci.yml
+ln -s $DOTFILES_DIR/.prettierrc.yml $HOME/.prettierrc.yml
+ln -s $DOTFILES_DIR/.tool-versions $HOME/.tool-versions
+ln -s $DOTFILES_DIR/.vimrc $HOME/.vimrc
+ln -s $DOTFILES_DIR/.wslconfig $HOME/.wslconfig
