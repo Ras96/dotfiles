0298ec50be736875b6ba112a94d0fa8d2a7c5d04
Ras96 <66677201+Ras96@users.noreply.github.com>
2022-02-28 19:37:54 +0900

    Update symboliclinks.sh

--- scripts/symboliclinks.sh
+++ scripts/symboliclinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOTFILES_REPO=/home/ras/ghq/github.com/Ras96/dotfiles
+DOTFILES_DIR=/home/ras/ghq/github.com/Ras96/dotfiles
 
 mkdir -p $HOME/.config/
 mkdir -p $HOME/.vscode-server/data/Machine/
