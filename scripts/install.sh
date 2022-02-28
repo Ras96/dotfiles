df99a6c90afaeca3eff17b8b55450da56c3bc871
Ras96 <66677201+Ras96@users.noreply.github.com>
2022-02-28 18:42:38 +0900

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -42,7 +42,7 @@ fi
 ghq get $DOTFILES_REPO_URL
 
 # シンボリックリンクを貼る
-$DOTFILES_REPO/scripts/symboliclinks.sh
+$(ghq root)/$DOTFILES_REPO/scripts/symboliclinks.sh
 
 # wsl.confのコピー
 sudo cp $(ghq root)/$DOTFILES_REPO/wsl.conf /etc/wsl.conf
