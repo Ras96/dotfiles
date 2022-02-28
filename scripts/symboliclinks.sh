50eba2f9531484c1796cb8be39c6b73659b71415
Ras96 <66677201+Ras96@users.noreply.github.com>
2022-02-28 18:39:49 +0900

    Update symboliclinks.sh

--- scripts/symboliclinks.sh
+++ scripts/symboliclinks.sh
@@ -3,15 +3,18 @@
 DOTFILES_REPO=Ras96/dotfiles
 DOTFILES_DIR=$(ghq root)/$DOTFILES_REPO
 
-ln -s $DOTFILES_DIR/.config/bat $HOME/.config/bat
-ln -s $DOTFILES_DIR/.config/fish $HOME/.config/fish
-ln -s $DOTFILES_DIR/.config/gitmoji-nodejs $HOME/.config/gitmoji-nodejs
-ln -s $DOTFILES_DIR/.config/yarn $HOME/.config/yarn
-ln -s $DOTFILES_DIR/.wincmd $HOME/.wincmd
-ln -s $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
-ln -s $DOTFILES_DIR/.golangci.yml $HOME/.golangci.yml
-ln -s $DOTFILES_DIR/.prettierrc.yml $HOME/.prettierrc.yml
-ln -s $DOTFILES_DIR/.tool-versions $HOME/.tool-versions
-ln -s $DOTFILES_DIR/.vimrc $HOME/.vimrc
-ln -s $DOTFILES_DIR/.wslconfig $HOME/.wslconfig
-ln -s $DOTFILES_DIR/.vscode-server/data/Machine/settings.json $HOME/.vscode-server/data/Machine/settings.json
+mkdir -p $HOME/.config/
+mkdir -p $HOME/.vscode-server/data/Machine/
+
+ln -sf $DOTFILES_DIR/.config/bat $HOME/.config/bat
+ln -sf $DOTFILES_DIR/.config/fish $HOME/.config/fish
+ln -sf $DOTFILES_DIR/.config/gitmoji-nodejs $HOME/.config/gitmoji-nodejs
+ln -sf $DOTFILES_DIR/.config/yarn $HOME/.config/yarn
+ln -sf $DOTFILES_DIR/.wincmd $HOME/.wincmd
+ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES_DIR/.golangci.yml $HOME/.golangci.yml
+ln -sf $DOTFILES_DIR/.prettierrc.yml $HOME/.prettierrc.yml
+ln -sf $DOTFILES_DIR/.tool-versions $HOME/.tool-versions
+ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
+ln -sf $DOTFILES_DIR/.wslconfig $HOME/.wslconfig
+ln -sf $DOTFILES_DIR/.vscode-server/data/Machine/settings.json $HOME/.vscode-server/data/Machine/settings.json
