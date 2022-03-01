800c9d7fd94b28001cf3f67aec3fa87192eaba53
Ras96 <asymptote.k.k@gmail.com>
2022-03-01 22:26:56 +0900

    :recycle: Update install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,57 +1,60 @@
-#!/bin/sh
+#!/bin/bash
 
-DOTFILES_REPO=/home/ras/ghq/github.com/Ras96/dotfiles
-DOTFILES_REPO_URL=https://github.com/Ras96/dotfiles.git
+DOTFILES_DIR=~/ghq/github.com/Ras96/dotfiles
+DOTFILES_REPO=git@github.com:Ras96/dotfiles.git
 
-# セットアップ & 基本コマンドのインストール
+function ec () { echo -e "\e[36;1m$1\e[m" }
+
+if [ ! -d "$HOME/.ssh" ]; then
+  ec "SSH configuration is required."
+  exit 1
+fi
+
+ec "Installing basic commands..."
 yes | sudo apt update
 yes | sudo apt upgrade
 yes | sudo add-apt-repository ppa:git-core/ppa
 yes | sudo apt install build-essential curl file git procps
 
-# brewのインストール
+if [ -d $DOTFILES_DIR ]; then
+  ec "Updating dotfiles repository..."
+  git -C $DOTFILES_DIR pull
+else
+  ec "Cloning dotfiles repository..."
+  mkdir -p $DOTFILES_DIR
+  git clone $DOTFILES_REPO $DOTFILES_DIR
+fi
+
+ec "Creating symboliclinks..."
+source $DOTFILES_DIR/scripts/symboliclinks.sh
+
 if !(type "brew" > /dev/null 2>&1); then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  ec "Installing brew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
 fi
 
-# 各種ツールのインストール
-# なるべくbrewで入れるようにする
-brew tap jandedobbeleer/oh-my-posh
-brew install \
-  asdf \
-  bat \
-  exa \
-  fish \
-  fzf \
-  gh \
-  ghq \
-  git-delta \
-  gitmoji \
-  go \
-  jq \
-  node \
-  oh-my-posh \
-  tig
-# FIXME: nodeとの依存関係があってよくないっぽいので個別で入れる
-brew install yarn --ignore-dependencies
-  
-if !(type "rustc" > /dev/null 2>&1); then
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-fi
+ec "Installing brew packages..."
+brew doctor
+brew update
+brew upgrade
+brew bundle --global
+brew bundle cleanup --global
 
-# レポジトリのクローン
-ghq get $DOTFILES_REPO_URL
-
-# シンボリックリンクを貼る
-$DOTFILES_REPO/scripts/symboliclinks.sh
-
-# wsl.confのコピー
-sudo cp $DOTFILES_REPO/wsl.conf /etc/wsl.conf
-
-# asdfプラグインのインストール
+ec "Installing asdf plugins..."
 cat ~/.tool-versions | awk '{print "asdf plugin add " $1}' | sh
+
+ec "Installing asdf packages..."
 asdf install
 
-sudo sh -c "echo $(which fish) >> /etc/shells"
+# if !(type "rustup" > /dev/null 2>&1); then
+#   ec "Installing rustup, rustc and cargo..."
+#   /bin/bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)"
+# fi
+
+ec "Setting up /etc ..."
+sudo cp $(ghq root)/$DOTFILES_REPO/wsl.conf /etc/wsl.conf
+sudo bash -c "echo "'nameserver 8.8.8.8'" > /etc/resolv.conf"
+sudo bash -c "echo $(which fish) >> /etc/shells"
+
 sudo chsh -s $(which fish)
--- scripts/symboliclinks.sh
+++ scripts/symboliclinks.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-DOTFILES_DIR=/home/ras/ghq/github.com/Ras96/dotfiles
+DOTFILES_DIR=$(ghq root)/github.com/Ras96/dotfiles
 
-mkdir -p $HOME/.config/
-mkdir -p $HOME/.vscode-server/data/Machine/
+mkdir -p $HOME/.config
+mkdir -p $HOME/.vscode-server/data/Machine
 
-ln -sf $DOTFILES_DIR/.config/bat $HOME/.config/bat
-ln -sf $DOTFILES_DIR/.config/fish $HOME/.config/fish
-ln -sf $DOTFILES_DIR/.config/gitmoji-nodejs $HOME/.config/gitmoji-nodejs
-ln -sf $DOTFILES_DIR/.config/yarn $HOME/.config/yarn
-ln -sf $DOTFILES_DIR/.wincmd $HOME/.wincmd
-ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
-ln -sf $DOTFILES_DIR/.golangci.yml $HOME/.golangci.yml
-ln -sf $DOTFILES_DIR/.prettierrc.yml $HOME/.prettierrc.yml
-ln -sf $DOTFILES_DIR/.tool-versions $HOME/.tool-versions
-ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
-ln -sf $DOTFILES_DIR/.wslconfig $HOME/.wslconfig
-ln -sf $DOTFILES_DIR/.vscode-server/data/Machine/settings.json $HOME/.vscode-server/data/Machine/settings.json
+ln -s $DOTFILES_DIR/.config/bat $HOME/.config/bat
+ln -s $DOTFILES_DIR/.config/fish $HOME/.config/fish
+ln -s $DOTFILES_DIR/.config/gitmoji-nodejs $HOME/.config/gitmoji-nodejs
+ln -s $DOTFILES_DIR/.config/yarn $HOME/.config/yarn
+ln -s $DOTFILES_DIR/.wincmd $HOME/.wincmd
+ln -s $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES_DIR/.golangci.yml $HOME/.golangci.yml
+ln -s $DOTFILES_DIR/.prettierrc.yml $HOME/.prettierrc.yml
+ln -s $DOTFILES_DIR/.tool-versions $HOME/.tool-versions
+ln -s $DOTFILES_DIR/.vimrc $HOME/.vimrc
+ln -s $DOTFILES_DIR/.wslconfig $HOME/.wslconfig
+ln -s $DOTFILES_DIR/.vscode-server/data/Machine/settings.json $HOME/.vscode-server/data/Machine/settings.json
+ln -s $DOTFILES_DIR/.Brewfile $HOME/.Brewfile
